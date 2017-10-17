b1cb39262f177a145c2f038421278df632202c12
Dige Ai <627766098@qq.com>
2017-10-17 16:42:38 +0800

    Update acnn_train.py

--- acnn_train.py
+++ acnn_train.py
@@ -15,9 +15,9 @@ NR = 19
 DC = 1000
 KP = 0.6
 K = 3
-LR = 0.2
+LR = 0.25
 BATCH_SIZE = 50
-epochs = 100
+epochs = 50
 data = pro.load_data('/home/dm/adg/remotetest/attention/train.txt')
 t_data = pro.load_data('/home/dm/adg/remotetest/attention/test.txt')
 word_dict = pro.build_dict(data[0])
@@ -25,6 +25,10 @@ x, y, e1, e2, dist1, dist2 = pro.vectorize(data, word_dict, N)
 y = np.array(y).astype(np.int64)
 np_cat = np.concatenate((x, np.array(e1).reshape(-1, 1), np.array(e2).reshape(-1, 1), np.array(dist1), np.array(dist2)),
                         1)
+e_x, e_y, e_e1, e_e2, e_dist1, e_dist2 = pro.vectorize(t_data, word_dict, N)
+y = np.array(y).astype(np.int64)
+eval_cat = np.concatenate(
+    (e_x, np.array(e_e1).reshape(-1, 1), np.array(e_e2).reshape(-1, 1), np.array(e_dist1), np.array(e_dist2)), 1)
 
 tx, ty, te1, te2, td1, td2 = pro.vectorize(t_data, word_dict, N)
 embed_file = '/home/dm/adg/remotetest/attention/embeddings.txt'
@@ -32,7 +36,7 @@ vac_file = '/home/dm/adg/remotetest/attention/words.lst'
 embedding = pro.load_embedding(embed_file, vac_file, word_dict)
 
 model = pa.ACNN(N, embedding, DP, NP, K, NR, DC, KP).cuda()
-optimizer = torch.optim.SGD(model.parameters(), lr=LR, weight_decay=0.0001)  # optimize all rnn parameters
+optimizer = torch.optim.SGD(model.parameters(), lr=LR)  # optimize all rnn parameters
 loss_func = pa.NovelDistanceLoss(NR)
 
 
@@ -62,42 +66,37 @@ def prediction(wo, rel_weight, y, NR):
     return (acc * 100).cpu().data.numpy()[0]
 
 
-k_fold = KFold(n_splits=10)
-
 for i in range(epochs):
-    split = 0
-    for train_indices, test_indices in k_fold.split(np_cat):
-        acc = 0
-        loss = 0
-        train = torch.from_numpy(np_cat[train_indices].astype(np.int64))
-        y_tensor = torch.LongTensor(y[train_indices])
-        train_datasets = D.TensorDataset(data_tensor=train, target_tensor=y_tensor)
-        train_dataloader = D.DataLoader(train_datasets, BATCH_SIZE, True, num_workers=2)
-        j = 0
-        for (b_x_cat, b_y) in train_dataloader:
-            bx, be1, be2, bd1, bd2, by = data_unpack(b_x_cat, b_y)
-            wo, rel_weight = model(bx, be1, be2, bd1, bd2)
-            acc += prediction(wo, rel_weight, by, NR)
-            l = loss_func(wo, rel_weight, by)
-            j += 1
-            optimizer.zero_grad()
-            l.backward()
-            optimizer.step()
-            loss += l
-        split += 1
-        print('epoch:', i, 'split:', split, 'acc:', acc / j, '%   loss:', loss.cpu().data.numpy()[0] / j)
-        test = torch.from_numpy(np_cat[test_indices].astype(np.int64))
-        ty_tensor = torch.LongTensor(y[test_indices])
-        test_datasets = D.TensorDataset(data_tensor=test, target_tensor=ty_tensor)
-        test_dataloader = D.DataLoader(train_datasets, BATCH_SIZE, True, num_workers=2)
-        test_acc = 0
-        ti = 0
-        t_loss = 0
-        for (b_tx_cat, b_ty) in test_dataloader:
-            btx, bte1, bte2, btd1, btd2, b_ty = data_unpack(b_tx_cat, b_ty)
-            two, t_rel_weight = model(btx, bte1, bte2, btd1, btd2, is_training=False)
-            test_acc += prediction(two, t_rel_weight, b_ty, NR)
-            # tl = loss_func(two, t_rel_weight, b_ty)
-            # t_loss += tl
-            ti += 1
-        print('epoch:', i, 'split:', split, 'test_acc:', test_acc / ti, '%')#   loss:', t_loss.cpu().data.numpy()[0] / ti)
+    acc = 0
+    loss = 0
+    train = torch.from_numpy(np_cat.astype(np.int64))
+    y_tensor = torch.LongTensor(y)
+    train_datasets = D.TensorDataset(data_tensor=train, target_tensor=y_tensor)
+    train_dataloader = D.DataLoader(train_datasets, BATCH_SIZE, True, num_workers=2)
+    j = 0
+    for (b_x_cat, b_y) in train_dataloader:
+        bx, be1, be2, bd1, bd2, by = data_unpack(b_x_cat, b_y)
+        wo, rel_weight = model(bx, be1, be2, bd1, bd2)
+        acc += prediction(wo, rel_weight, by, NR)
+        l = loss_func(wo, rel_weight, by)
+        j += 1
+        optimizer.zero_grad()
+        l.backward()
+        optimizer.step()
+        loss += l
+    eval = torch.from_numpy(eval_cat.astype(np.int64))
+    eval_acc = 0
+    ti = 0
+    y_tensor = torch.LongTensor(e_y)
+    eval_datasets = D.TensorDataset(data_tensor=eval, target_tensor=y_tensor)
+    eval_dataloader = D.DataLoader(eval_datasets, BATCH_SIZE, True, num_workers=2)
+    for (b_x_cat, b_y) in eval_dataloader:
+        bx, be1, be2, bd1, bd2, by = data_unpack(b_x_cat, b_y)
+        wo, rel_weight = model(bx, be1, be2, bd1, bd2)
+        eval_acc += prediction(wo, rel_weight, by, NR)
+        ti += 1
+    print('epoch:', i, 'acc:', acc / j, '%   loss:', loss.cpu().data.numpy()[0] / j, 'test_acc:', eval_acc / ti, '%')
+
+torch.save(model.state_dict(), 'acnn_params.pkl')
+# eval = torch.from_numpy(np_cat.astype(np.int64))
+# model.load_state_dict(torch.load('acnn_params.pkl'))
